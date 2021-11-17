851af06cad856a2150ea8ea5062d0a6db40b8466
scherbakov <61695234+AndrewSherbakov@users.noreply.github.com>
2021-11-17 13:39:54 +0300

    Правки

---
+++ js/user-authorization.js
@@ -0,0 +1,8 @@
+<script>
+  enterLink = document.getElementByClassName('.navigation-user-item-login');
+  modalWindow = document.getElementByClassName('.modal-container');
+
+  enterLink.onclick() = function() {
+    modalWindow.classList.add('.modal-container-show')
+  }
+</script>
